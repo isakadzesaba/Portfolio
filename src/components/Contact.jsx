d327a9215fe0e85d0466b50fa306870c2c8d53c7
isakadzesaba <sabaisakadze99@gmail.com>
2024-12-26 23:21:06 +0400

    commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -63,7 +63,7 @@ const Contact = () => {
         </div>
 
         <form
-          action="https://getform.io/f/bgddnpya"
+          action="https://getform.io/f/amddwvdb"
           method="POST"
           className="xl:pl-10 2xl:pl-20"
         >
